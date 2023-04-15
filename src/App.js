48a9973be25d05cf22d4602ea3bbd2689733a029
raobaba <raorajan9576@gmail.com>
2023-04-15 07:50:57 +0530

    delete detail

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>
-          I was building here a todo application with the help of 
-          MERN
-          M-mongodb
-          E-express
-          R-react
-          N-nodejs
-        </h1>
+        Todo-Crud-Application
       </div>
     </div>
   );
