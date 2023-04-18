19c6592cebc65e5fb6ee0fad202dea0b400b1e4b
raobaba <raorajan9576@gmail.com>
2023-04-18 12:48:22 +0530

    todo component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Header';
+import Todo from './Components/Todo';
 import TodoForm from './Components/TodoForm';
 function App() {
   return (
     <div>
       <Header />
       <TodoForm />
+      <Todo/>
     </div>
   );
 }
