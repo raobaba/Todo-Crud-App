3f7db4fff8c520e37fb57a709f03a7c86a52e6d8
raobaba <raorajan9576@gmail.com>
2023-04-19 15:20:40 +0530

    tabs added

--- src/Redux/Action.js
+++ src/Redux/Action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO } from './actionType.js';
+import { ADDNEW_TODO, DELETE_TODO, GETALL_TODO, TOGGLE_TODO, UPDATE_TODO } from './actionType.js';
 const API_URL = 'http://localhost:8000';
 export const addNewTodo = (data) => async (dispatch) => {
     try {
@@ -30,8 +30,17 @@ export const toggleTodo = (id) => async (dispatch) => {
 export const updateTodo = (id,data) => async (dispatch) => {
     try {
         const res = await axios.put(`${API_URL}/todos/${id}`,{data});
-        dispatch({ type: TOGGLE_TODO, payload: res.data })
+        dispatch({ type: UPDATE_TODO, payload: res.data })
     } catch (error) {
-        console.log("Error while calling toggleTodo", error.messasge);
+        console.log("Error while calling upateTodo", error.messasge);
+    }
+}
+
+export const deleteTodo = (id) => async (dispatch) => {
+    try {
+        const res = await axios.delete(`${API_URL}/todos/${id}`);
+        dispatch({ type: DELETE_TODO, payload: res.data })
+    } catch (error) {
+        console.log("Error while calling deleteTodo", error.messasge);
     }
 }
